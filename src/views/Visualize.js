3610a889baeda3fb8db444176565736485b4d936
Vandan Rogheliya <rogheliavandan@outlook.com>
2020-11-10 14:19:03 +0530

    Add Comments to Visualize.js

--- src/views/Visualize.js
+++ src/views/Visualize.js
@@ -1,27 +1,15 @@
 import React, { useState } from 'react'
+
+// Importing graph
 import Graph from 'react-graph-vis'
 import { Button } from 'reactstrap'
 
+// Defining Visualize component
 function Visualize({ data, setHome }) {
 	const [graph, setGraph] = useState(-1)
 
-	const graph1 = {
-		nodes: [
-			{ id: 'L10', label: 'Long', size: 50 },
-			{ id: 'M20', label: 'Monthly', size: 25 },
-			{ id: 'W30', label: 'Weekly', size: 15 },
-			{ id: 'T40', label: 'Task', size: 10 },
-			// { id: 5, label: 'Node 5', title: 'node 5 tootip text' },
-		],
-		edges: [
-			{ from: 'L10', to: 'M20' },
-			{ from: 'W30', to: 'L10' },
-			{ from: 'W30', to: 'T40' },
-		],
-	}
-
+	// Setting options for Graph
 	const options = {
-		// autoResize: true,
 		height: '760px',
 		width: '100%',
 		layout: {
@@ -37,21 +25,16 @@ function Visualize({ data, setHome }) {
 		},
 	}
 
-	const events = {
-		select: function (event) {
-			var { nodes, edges } = event
-		},
-	}
-
+	// Making Graph
 	const makeGraph = () => {
-		console.log(data)
-
 		var nodes = []
 		var edges = []
 
 		for (const type in data) {
-			// console.log(type);
-			let size, letter, parentLetter, mass = 1
+			let size,
+				letter,
+				parentLetter,
+				mass = 1
 			if (type === 'long') {
 				size = 40
 				letter = 'L'
@@ -85,32 +68,21 @@ function Visualize({ data, setHome }) {
 
 			if (parentLetter !== null) {
 				for (let key in data[type]) {
-					// console.log(data[type][key].parent_goal, parentLetter)
 					if (data[type][key].parent_goal !== null) {
 						edges.push({ from: parentLetter + data[type][key].parent_goal, to: letter + key })
 					}
-					// { id: letter + key, label: data[type][key].topic, size }
 				}
 			}
 		}
-		console.log(nodes, edges)
 		setGraph({ nodes, edges })
 	}
 
+	// Make graph if not already made
 	if (graph === -1) makeGraph()
 
 	return (
 		<div>
-			{graph !== -1 && (
-				<Graph
-					graph={graph}
-					options={options}
-					events={events}
-					getNetwork={network => {
-						//  if you want access to vis.js network api you can set the state in a parent component using this property
-					}}
-				/>
-			)}
+			{graph !== -1 && <Graph graph={graph} options={options} />}
 			<Button color="default" className="btn-round  back-btn" onClick={() => setHome(1)}>
 				<i className="tim-icons icon-double-left" />
 			</Button>
