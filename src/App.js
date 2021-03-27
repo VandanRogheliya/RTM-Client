016786d05d2c453ed2098f6ed5deb87d463316eb
Vandan Rogheliya <rogheliavandan@outlook.com>
2021-03-27 21:07:15 +0530

    use useEffect to fetch data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 // Styles
 import './assets/css/nucleo-icons.css'
@@ -16,79 +16,85 @@ import config from './config/config'
 
 // App Component
 function App() {
+  // Loading State
+  const [isLoading, setIsLoading] = useState(true)
 
-	// Loading State
-	const [isLoading, setIsLoading] = useState(true)
+  // Data State
+  const [data, setData] = useState({
+    task: -1,
+    week: -1,
+    month: -1,
+    long: -1,
+  })
 
-	// Data State
-	const [data, setData] = useState({
-		task: -1,
-		week: -1,
-		month: -1,
-		long: -1,
-	})
+  // Home State, if true shows Home else Mission
+  const [home, setHome] = useState(1)
 
-	// Home State, if true shows Home else Mission 
-	const [home, setHome] = useState(1)
+  // States of goal/task for Mission component
+  const [type, setType] = useState('')
+  const [id, setId] = useState(0)
 
-	// States of goal/task for Mission component
-	const [type, setType] = useState('')
-	const [id, setId] = useState(0)
+  // Fetching All Data
+  const getData = async () => {
+    var taskData = new Map()
+    var weekData = new Map()
+    var monthData = new Map()
+    var longData = new Map()
+    var task = await fetch(`${config.api}/task`)
+    task = await task.json()
 
-	// Fetching All Data
-	const getData = async () => {
-		var taskData = new Map()
-		var weekData = new Map()
-		var monthData = new Map()
-		var longData = new Map()
-		var task = await fetch(`${config.api}/task`)
-		task = await task.json()
+    var week = await fetch(`${config.api}/week`)
+    week = await week.json()
 
-		var week = await fetch(`${config.api}/week`)
-		week = await week.json()
+    var month = await fetch(`${config.api}/month`)
+    month = await month.json()
 
-		var month = await fetch(`${config.api}/month`)
-		month = await month.json()
+    var long = await fetch(`${config.api}/long`)
+    long = await long.json()
 
-		var long = await fetch(`${config.api}/long`)
-		long = await long.json()
+    // Storing each task/goal in form of map. Key -> ID and Value -> task/goal object
+    task.forEach((e) => (taskData[e.id] = e))
+    week.forEach((e) => (weekData[e.id] = e))
+    month.forEach((e) => (monthData[e.id] = e))
+    long.forEach((e) => (longData[e.id] = e))
 
-		// Storing each task/goal in form of map. Key -> ID and Value -> task/goal object
-		task.forEach(e => (taskData[e.id] = e))
-		week.forEach(e => (weekData[e.id] = e))
-		month.forEach(e => (monthData[e.id] = e))
-		long.forEach(e => (longData[e.id] = e))
+    // Changing State
+    setData({
+      task: taskData,
+      week: weekData,
+      month: monthData,
+      long: longData,
+    })
 
-		// Changing State
-		setData({
-			task: taskData,
-			week: weekData,
-			month: monthData,
-			long: longData,
-		})
+    // Setting Loading to false
+    setIsLoading(false)
+  }
 
-		// Setting Loading to false
-		setIsLoading(false)
-	}
+  // If data has not yet been fetch, then fetch it
+  // if (data.task === -1)
+  useEffect(() => {
+    getData()
+  }, [])
 
-	// If data has not yet been fetch, then fetch it
-	if (data.task === -1) getData()
-	
-	// Rendering JSX
-	return (
-		<div className="App">
-			{isLoading ? (
-				<h1>Loading...</h1>
-			) : home === 1 ? (
-				<Home data={data} setType={e => setType(e)} setId={e => setId(e)} setHome={e => setHome(e)} />
-			) : home === 0 ? (
-				<Mission type={type} id={id} data={data} setHome={e => setHome(e)} />
-			) : 
-			<Visualize data={data} setHome={e => setHome(e)}/> 
-			}
-
-		</div>
-	)
+  // Rendering JSX
+  return (
+    <div className='App'>
+      {isLoading ? (
+        <h1>Loading...</h1>
+      ) : home === 1 ? (
+        <Home
+          data={data}
+          setType={(e) => setType(e)}
+          setId={(e) => setId(e)}
+          setHome={(e) => setHome(e)}
+        />
+      ) : home === 0 ? (
+        <Mission type={type} id={id} data={data} setHome={(e) => setHome(e)} />
+      ) : (
+        <Visualize data={data} setHome={(e) => setHome(e)} />
+      )}
+    </div>
+  )
 }
 
 export default App
